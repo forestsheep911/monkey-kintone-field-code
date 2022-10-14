1c51ab4cc6c35db03e7ec2a74ef3a10d553255dd
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2022-10-14 10:24:51 +0800

    update

--- config/dev.meta.js
+++ config/dev.meta.js
@@ -3,7 +3,15 @@ const { resolve } = require('path')
 module.exports = {
   name: pj.name,
   namespace: `https://github.com/${pj.author}/monkey-${pj.name}`,
-  match: ['http://localhost:8080'],
-  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener'],
-  require: [`file://${resolve(__dirname, '../')}/dist/${pj.name}.dev.user.js`],
+  match: [
+    'https://*.cybozu.cn/k/*/show*',
+    'https://*.cybozu.com/k/*/show*',
+    'https://*.cybozu-dev.com/k/*/show*',
+    'https://*.kintone.com/k/*/show*',
+    'https://*.s.cybozu.cn/k/*/show*',
+    'https://*.s.cybozu.com/k/*/show*',
+    'https://*.s.kintone.com/k/*/show*',
+  ],
+  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener', 'GM_addStyle'],
+  require: [`file://${resolve(__dirname, '../dist/').replaceAll('\\', '/')}${pj.name}.dev.user.js`],
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,69 @@
+const Swal = require('sweetalert2')
 const app = () => {
-  console.log('monkey jumping on the bed.')
+  GM_addStyle(`
+        #swal2-title {
+            text-align: center;
+        }
+    `)
+  const params = {
+    app: kintone.app.getId(),
+    lang: 'user',
+  }
+  async function getFields() {
+    const res = await kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params)
+    return res
+  }
+  function insertAfter(newNode: Node, referenceNode: Node) {
+    if (referenceNode.parentNode) referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
+  }
+  async function mainWork() {
+    const objFields = await getFields()
+    const eleCommonLabels: NodeListOf<HTMLElement> = document.querySelectorAll('.control-label-gaia, .group-label-gaia')
+    let showCode: string
+    let defaultBGColor: string
+    for (let i = 0; i < eleCommonLabels.length; i += 1) {
+      eleCommonLabels[i].style.cursor = 'copy'
+      eleCommonLabels[i].onmouseover = () => {
+        defaultBGColor = eleCommonLabels[i].style.backgroundColor
+        eleCommonLabels[i].style.backgroundColor = '#f2b36f'
+        Object.keys(objFields.properties).forEach((key) => {
+          if (eleCommonLabels[i].innerText === objFields.properties[key].label) {
+            showCode = objFields.properties[key].code
+          }
+        })
+        Swal.fire({
+          title: showCode,
+          toast: true,
+          position: 'bottom',
+          showConfirmButton: false,
+          width: 520,
+          padding: `0em`,
+          timer: 5000,
+        })
+      }
+      eleCommonLabels[i].onmouseout = () => {
+        Swal.close()
+        eleCommonLabels[i].style.backgroundColor = defaultBGColor
+      }
+      eleCommonLabels[i].onclick = async () => {
+        await navigator.clipboard.writeText(showCode)
+        Swal.fire({
+          title: `<span style="color:#129459">${showCode}</span><br>已拷贝至剪切板`,
+          toast: true,
+          position: 'top',
+          showConfirmButton: false,
+          width: 360,
+          padding: `0em`,
+          timer: 3000,
+        })
+        // }
+      }
+    }
+  }
+  kintone.events.on('app.record.detail.show', function (event) {
+    mainWork()
+    return event
+  })
 }
 
 export default app
